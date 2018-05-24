f267623401422b55d7cf9e31d509d85b439fd72b
Shushalsec <shtoneyan@gmail.com>
2018-05-24 14:22:06 +0200

    Update knnValidation.py

--- knnValidation.py
+++ knnValidation.py
@@ -51,7 +51,7 @@ def main():
         kNN_GED = getkNN_GED(graphs_training, train_label_keys, unknown_graph, 5)
         # Get the label occuring the most among the k nearest neighbors
         result_label = getResponse(train_labels, kNN_GED)
-        output.write("{}\t\{}\n".format(valid_ids[id_index], result_label))
+        output.write("{}\t{}\n".format(valid_ids[id_index], result_label))
         output.flush()
     output.close()
     # Get the accuracy. Compares test_label with prediction_label. How well is the testing set?
